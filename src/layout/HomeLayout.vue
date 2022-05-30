f5b5332eccc2e319712fff62592cd0ee20e84383
raihan4816 <raihan4816@gmail.com>
2022-05-30 18:19:30 +0700

    update button href/routing to home layout

--- src/layout/HomeLayout.vue
+++ src/layout/HomeLayout.vue
@@ -2,7 +2,7 @@
   <div id="home-layout">
     <div class="home-layout-navbar">
       <b-navbar toggleable="lg" variant="faded" type="light" class="p-4">
-        <b-navbar-brand href="#">LearnITnow</b-navbar-brand>
+        <b-navbar-brand href="/">LearnITnow</b-navbar-brand>
 
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
@@ -10,15 +10,15 @@
           
           <!-- Right aligned nav items -->
           <b-navbar-nav class="m-auto">
-            <b-nav-item href="#" class="mx-3">Home</b-nav-item>
-            <b-nav-item href="#" class="mx-3">Programs</b-nav-item>
-            <b-nav-item href="#" class="mx-3">Courses</b-nav-item>
-            <b-nav-item href="#" class="mx-3">Contact</b-nav-item>
+            <b-nav-item href="/" class="mx-3">Home</b-nav-item>
+            <b-nav-item href="programs" class="mx-3">Programs</b-nav-item>
+            <b-nav-item href="courses" class="mx-3">Courses</b-nav-item>
+            <b-nav-item href="contact" class="mx-3">Contact</b-nav-item>
           </b-navbar-nav>
           
           <b-navbar-nav>
-            <b-nav-item href="#">Login</b-nav-item>
-            <b-button variant="info" class="p-2">Sign Up</b-button>
+            <b-nav-item href="login">Login</b-nav-item>
+            <b-button variant="info" class="p-2" href="login">Sign Up</b-button>
           </b-navbar-nav>
 
         </b-collapse>
