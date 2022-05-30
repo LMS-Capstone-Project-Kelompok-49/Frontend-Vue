8b2d7d7be65f9509ac52a39af5c07f42a1a5471a
raihan4816 <raihan4816@gmail.com>
2022-05-30 18:21:03 +0700

    add Login/Register Page

---
+++ src/views/LoginRegisterView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="loginRegister">
+    Login Register Component Here
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginRegisterView',
+  components: {
+  }
+}
+</script>
