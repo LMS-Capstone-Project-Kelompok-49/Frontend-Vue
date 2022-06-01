b12c26dea1dd45647d28048da4746196280804b9
raihan4816 <raihan4816@gmail.com>
2022-06-01 14:24:34 +0700

    add landing page component, update homepage, add assets

---
+++ src/components/landingpage/SecTitleItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="title pt-5">
+        <h1 class="titlecolor">Why we Learn</h1>
+        <h1 class="titlecolor">at LearnITNow</h1>
+        <p class="pt-4">Akses yang dapat dilakukan dimanapun dengan berbagai sumber pembelajaran, mulai dari video pembelajaran, materi dan kelas bersama mentor yang berpengalaman.</p>
+        <b-button :to="{ name: 'RegisterPage' }" variant="info" class="p-2 mt-3 fw-semibold"> Try Now </b-button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'SecTitleItem'
+}
+</script>
+
+<style scoped>
+.titlecolor {
+   color: #3D3D3D; 
+}
+</style>
