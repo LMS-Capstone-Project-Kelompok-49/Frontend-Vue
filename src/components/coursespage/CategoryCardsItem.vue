c4d08cf89c0ee67f324487eb6e722ba642c4f1eb
raihan4816 <raihan4816@gmail.com>
2022-06-01 19:38:14 +0700

    add Courses page

---
+++ src/components/coursespage/CategoryCardsItem.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="cards pt-5 mt-5">
+        <h2 class="titlecolor mb-5 mt-5">Our Learning Opportunity</h2>
+        <div class="d-flex text-center justify-content-center align-items-center">
+            <div class="card bg-dark text-white mr-5 border-light">
+                <img :src="require('@/assets/images/image_card.jpg')" class="card-img" alt="...">
+                <div class="card-img-overlay">
+                    <h5 class="card-title m-4">Frontend</h5>
+                </div>
+            </div>
+            <div class="card bg-dark text-white ml-5 mr-5 border-light">
+                <img :src="require('@/assets/images/image_card.jpg')" class="card-img" alt="...">
+                <div class="card-img-overlay">
+                    <h5 class="card-title m-4">Backend</h5>
+                </div>
+            </div>
+            <div class="card bg-dark text-white mr-5 ml-5 border-light">
+                <img :src="require('@/assets/images/image_card.jpg')" class="card-img" alt="...">
+                <div class="card-img-overlay">
+                    <h5 class="card-title m-2">Quality Engineer</h5>
+                </div>
+            </div>
+            <div class="card bg-dark text-white ml-5 border-light">
+                <img :src="require('@/assets/images/image_card.jpg')" class="card-img" alt="...">
+                <div class="card-img-overlay m-4">
+                    <h5 class="card-title">UI/UX</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CategoryCardsItem'
+}
+</script>
+
+<style scoped>
+.titlecolor {
+   color: #3D3D3D; 
+}
+</style>
--- src/views/CoursesView.vue
+++ src/views/CoursesView.vue
@@ -1,13 +1,35 @@
 <template>
-  <div class="courses">
-    Courses View
+  <div class="home container">
+    <div class="row mt-3">
+      <div class="col">
+        <HeroCourseItem />
+      </div>
+      <div class="col">
+        <TitleCourseItem />
+      </div>
+    </div>
+    <div>
+      <CategoryCardsItem />
+    </div>
+    <div>
+      <PopularCourseItem />
+    </div>
   </div>
 </template>
 
 <script>
+import TitleCourseItem from "../components/coursespage/TitleCourseItem.vue"
+import HeroCourseItem from "../components/coursespage/HeroCourseItem.vue"
+import CategoryCardsItem from "../components/coursespage/CategoryCardsItem.vue"
+import PopularCourseItem from "../components/coursespage/PopularCourseItem.vue"
+
 export default {
   name: 'CoursesView',
   components: {
+    TitleCourseItem,
+    HeroCourseItem,
+    CategoryCardsItem,
+    PopularCourseItem,
   }
 }
 </script>
