55c5f8c89b2c9b123f2eba9491af93db5309e2fc
raihan4816 <raihan4816@gmail.com>
2022-06-15 08:57:29 +0700

    update component of dashboard page

---
+++ src/components/dashboard/MainDashboard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="main-dashboard">
+      <div class="welcome-dashboard pl-3 pt-3 pb-5">
+          <h2 class="title pt-3 pl-4 pr-5">Welcome Back Lutfhi</h2>
+          <h4 class="subtitle pl-4 pr-5">Kamu punya beberapa tugas yang harus diselesaikan hari ini!!!</h4>
+      </div>
+      <MainContent class="mt-5" />
+  </div>
+</template>
+
+<script>
+import MainContent from "@/components/dashboard/MainContent.vue";
+
+export default {
+  name: 'MainDashboard',
+  components: {
+      MainContent,
+  }
+}
+</script>
+
+<style scoped>
+.welcome-dashboard {
+    background: #00AFB9;
+    border-radius: 12px;
+    background-image: url("@/assets/images/bg_shadow_popup.png");
+}
+.title{
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 30px;
+    color: #FFFFFF;
+}
+.subtitle{
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 15px;
+    color: #FFFFFF;
+}
+</style>
