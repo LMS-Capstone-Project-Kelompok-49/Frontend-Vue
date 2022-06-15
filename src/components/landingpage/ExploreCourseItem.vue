9bb2cbac10a6982648d7f14053767150c9a9097f
VeronicaNatasya <veronicanatasya35@gmail.com>
2022-06-15 21:08:50 +0700

    Update ExploreCourseItem

--- src/components/landingpage/ExploreCourseItem.vue
+++ src/components/landingpage/ExploreCourseItem.vue
@@ -6,104 +6,104 @@
                 <div class="col">
                     <div class="card text-light border-light" style="width: 18rem;">
                         <div class="card-body">
-                            <div class="logo-image">
-                                <img :src="require('@/assets/images/logo_course.png')" width="50px" />
+                            <div class="rectangle4486">
+                                <img :src="require('@/assets/images/Rectangle4486.png')" width="70px" />
                             </div>
-                            <h5 class="card-title mt-3">Flutter Courses</h5>
-                            <h5 class="card-title">$100</h5>
+                            <h5 class="card-title mt-3">Mastering React JS untuk Pemula</h5>
                             <div class="d-flex justify-content-between">
-                                <p class="card-text">45 Videos</p>
-                                <p class="card-text">5k+ reviews</p>
+                                <p class="card-text">15 Videos</p>
+                                <p class="card-text">50+ Members</p>
                             </div>
-                            <a href="#" class="btn btn-light text-center">Join Now</a>
+                            <h5 class="card-title">Rp 520.000</h5>
+                            <a href="#" class="btn btn-light text-center">Join Class</a>
+                        </div>
+                    </div>  
+                </div>
+                <div class="col">
+                    <div class="card text-light border-light" style="width: 18rem;">
+                        <div class="card-body">
+                            <div class="rectangle4486">
+                                <img :src="require('@/assets/images/Rectangle4486.png')" width="70px" />
+                            </div>
+                            <h5 class="card-title mt-3">Mastering React JS untuk Pemula</h5>
+                            <div class="d-flex justify-content-between">
+                                <p class="card-text">15 Videos</p>
+                                <p class="card-text">50+ Members</p>
+                            </div>
+                            <h5 class="card-title">Rp 520.000</h5>
+                            <a href="#" class="btn btn-light text-center">Join Class</a>
                         </div>
                     </div>
                 </div>
                 <div class="col">
                     <div class="card text-light border-light" style="width: 18rem;">
                         <div class="card-body">
-                            <div class="logo-image">
-                                <img :src="require('@/assets/images/logo_course.png')" width="50px" />
+                            <div class="rectangle4486">
+                                <img :src="require('@/assets/images/Rectangle4486.png')" width="70px" />
                             </div>
-                            <h5 class="card-title mt-3">Flutter Courses</h5>
-                            <h5 class="card-title">$100</h5>
+                            <h5 class="card-title mt-3">Mastering React JS untuk Pemula</h5>
                             <div class="d-flex justify-content-between">
-                                <p class="card-text">45 Videos</p>
-                                <p class="card-text">5k+ reviews</p>
+                                <p class="card-text">15 Videos</p>
+                                <p class="card-text">50+ Members</p>
                             </div>
-                            <a href="#" class="btn btn-light text-center">Join Now</a>
+                            <h5 class="card-title">Rp 520.000</h5>
+                            <a href="#" class="btn btn-light text-center">Join Class</a>
                         </div>
                     </div>
                 </div>
-                <div class="col">
-                    <div class="card text-light border-light" style="width: 18rem;">
-                        <div class="card-body">
-                            <div class="logo-image">
-                                <img :src="require('@/assets/images/logo_course.png')" width="50px" />
-                            </div>
-                            <h5 class="card-title mt-3">Flutter Courses</h5>
-                            <h5 class="card-title">$100</h5>
-                            <div class="d-flex justify-content-between">
-                                <p class="card-text">45 Videos</p>
-                                <p class="card-text">5k+ reviews</p>
-                            </div>
-                            <a href="#" class="btn btn-light text-center">Join Now</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
             <div class="row mb-5">
                 <div class="col">
                     <div class="card text-light border-light" style="width: 18rem;">
                         <div class="card-body">
-                            <div class="logo-image">
-                                <img :src="require('@/assets/images/logo_course.png')" width="50px" />
+                            <div class="rectangle4486">
+                                <img :src="require('@/assets/images/Rectangle4486.png')" width="70px" />
                             </div>
-                            <h5 class="card-title mt-3">Flutter Courses</h5>
-                            <h5 class="card-title">$100</h5>
+                            <h5 class="card-title mt-3">Mastering React JS untuk Pemula</h5>
                             <div class="d-flex justify-content-between">
-                                <p class="card-text">45 Videos</p>
-                                <p class="card-text">5k+ reviews</p>
+                                <p class="card-text">15 Videos</p>
+                                <p class="card-text">50+ Members</p>
                             </div>
-                            <a href="#" class="btn btn-light text-center">Join Now</a>
+                            <h5 class="card-title">Rp 520.000</h5>
+                            <a href="#" class="btn btn-light text-center">Join Class</a>
                         </div>
                     </div>
                 </div>
                 <div class="col">
                     <div class="card text-light border-light" style="width: 18rem;">
                         <div class="card-body">
-                            <div class="logo-image">
-                                <img :src="require('@/assets/images/logo_course.png')" width="50px" />
+                            <div class="rectangle4486">
+                                <img :src="require('@/assets/images/Rectangle4486.png')" width="70px" />
                             </div>
-                            <h5 class="card-title mt-3">Flutter Courses</h5>
-                            <h5 class="card-title">$100</h5>
+                            <h5 class="card-title mt-3">Mastering React JS untuk Pemula</h5>
                             <div class="d-flex justify-content-between">
-                                <p class="card-text">45 Videos</p>
-                                <p class="card-text">5k+ reviews</p>
+                                <p class="card-text">15 Videos</p>
+                                <p class="card-text">50+ Members</p>
                             </div>
-                            <a href="#" class="btn btn-light text-center">Join Now</a>
+                            <h5 class="card-title">Rp 520.000</h5>
+                            <a href="#" class="btn btn-light text-center">Join Class</a>
                         </div>
                     </div>
                 </div>
                 <div class="col">
                     <div class="card text-light border-light" style="width: 18rem;">
                         <div class="card-body">
-                            <div class="logo-image">
-                                <img :src="require('@/assets/images/logo_course.png')" width="50px" />
+                            <div class="rectangle4486">
+                                <img :src="require('@/assets/images/Rectangle4486.png')" width="70px" />
                             </div>
-                            <h5 class="card-title mt-3">Flutter Courses</h5>
-                            <h5 class="card-title">$100</h5>
+                            <h5 class="card-title mt-3">Mastering React JS untuk Pemula</h5>
                             <div class="d-flex justify-content-between">
-                                <p class="card-text">45 Videos</p>
-                                <p class="card-text">5k+ reviews</p>
+                                <p class="card-text">15 Videos</p>
+                                <p class="card-text">50+ Members</p>
                             </div>
-                            <a href="#" class="btn btn-light text-center">Join Now</a>
+                            <h5 class="card-title">Rp 520.000</h5>
+                            <a href="#" class="btn btn-light text-center">Join Class</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 </template>
 
 <script>
@@ -113,16 +113,33 @@ export default {
 </script>
 
 <style scoped>
-.titlecolor {
-   color: #3D3D3D; 
+.titlecolor { 
+position: absolute;
+width: 348px;
+height: 48px;
+left: 80px;
+top: 2133px;
+
+font-family: 'Poppins';
+font-style: normal;
+font-weight: 700;
+font-size: 32px;
+line-height: 48px;
+/* identical to box height */
+/* onyx */
+
+color: #3D3D3D;
 }
-.logo-image {
-  width: 46px;
-  height: 46px;
-  border-radius: 50%;
-  overflow: hidden;
-  margin-top: -6px;
+.rectangle4486 {
+position: absolute;
+height: 70px;
+left: 5.5%;
+right: 77%;
+top: 22px;
+
+border-radius: 8px;
 }
+
 .card {
    background-color: #00AFB9; 
 }
